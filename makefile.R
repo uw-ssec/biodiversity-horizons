c4f422e4b5e8f186346b8ccf5d13dc592b84be5e
stlp <ishikakhandelwal02@gmail.com>
2024-10-24 15:09:10 -0700

    Add nolint to makefile and update pre-commit setup

--- makefile.R
+++ makefile.R
@@ -1,46 +1,27 @@
-
 #### Template of a master script for the project ####
 
 ## Using an advanced tool like {drake} or {targets} is recommended,
 ## but this can work as a simple alternative.
 
-
 ### if you have written specific functions in R/, load the package:
-# library("myproject")
-
+# library("myproject") # nolint
 
 ### Source data cleaning scripts
-# source("data-raw/datacleaning.R")
-### Could also be Rmarkdown:
-# rmarkdown::render("data-raw/datacleaning.Rmd")
-
+# source("data-raw/datacleaning.R") # nolint
+# rmarkdown::render("data-raw/datacleaning.Rmd") # nolint
 
 ### Render manuscript
-# rmarkdown::render("manuscript/ms_project.Rmd")
-
+# rmarkdown::render("manuscript/ms_project.Rmd") # nolint
 
 ## Check your code ##
-# goodpractice::gp()
-
-
+# goodpractice::gp() # nolint
 
 #### Control package dependencies ####
-
-# Many alternatives available, for example:
-
-# sessionInfo()
-
-# automagic::make_deps_file()
-
-# Or using renv:
-# renv::init()
-# renv::snapshot()
-
-# Or use Docker, see e.g. {containerit} or {liftr}
-
+# sessionInfo() # nolint
+# renv::init() # nolint
+# renv::snapshot() # nolint
 
 ## Make a website for your project?
 ## see https://pkgdown.r-lib.org/
-# usethis::use_pkgdown()
-# pkgdown::build_site()
-
+# usethis::use_pkgdown() # nolint
+# pkgdown::build_site() # nolint
