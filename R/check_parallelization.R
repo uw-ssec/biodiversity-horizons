7e355de81f62010051e8c1f3419db0a994e02d49
Cordero Core <127983572+uwcdc@users.noreply.github.com>
2024-12-19 23:18:20 +0000

    feat(check-parallelization): Outputs a boolean based on available cores, platform, and RStudio support

---
+++ R/check_parallelization.R
@@ -0,0 +1,21 @@
+check_parallelization <- function() {
+  # Check the number of physical cores
+  num_cores <- parallel::detectCores(logical = FALSE)  # Use physical cores only
+  
+  # If there are fewer than 2 physical cores, parallelization is not possible
+  if (num_cores < 2) {
+    return(FALSE)
+  }
+  
+  # Check if multicore parallelization is supported using parallelly package
+  # This accounts for the platform and RStudio environment
+  if (!parallelly::supportsMulticore()) {
+    return(FALSE)
+  }
+  
+  # If the system has multiple cores and supports multicore parallelization, return TRUE
+  return(TRUE)
+}
+
+# Example usage:
+check_parallelization_support()
