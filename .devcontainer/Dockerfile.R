7a6b75663bba926f2efd2393f7e8814732066b15
anujsinha3 <sinha.anuj30@gmail.com>
2025-02-25 14:42:21 -0800

    feat: build and push docker docker image to ghcr thru GH action (#89)
    * feat: test docker pipeline action
    
    * feat: specify correct path for dockerfile
    
    * feat: fix devtool not found
    
    * feat: fix devtool not found
    
    * feat: fix devtool not found
    
    * feat: fix devtool not found
    
    * feat: fix devtool not found
    
    * feat: fix devtool not found
    
    * feat: fix devtool not found
    
    * feat: fix devtool not found
    
    * feat: fix devtool not found
    
    * feat: fix devtool not found
    
    * feat: fix devtool not found rocker/r-ver
    
    * feat: fix devtool not found libproj-dev
    
    * feat: fix devtool not found libgdal-dev \
    
    * feat: fix devtool not found libudunits2-dev \
    
    * feat: fix devtool not found extract version
    
    * feat: test package fix - add sleep
    
    * feat: test package fix - add sleep
    
    * feat: test package fix - correct name
    
    * feat: test package fix - lataest
    
    * feat: build and push only
    
    * feat: build and push only - add push tag
    
    * feat: build and push with test
    
    * style: pre-commit lint fix
    
    * style: forbidden issue fix
    
    * style: forbidden issue fix
    
    * style: forbidden issue fix - permissions

---
+++ .devcontainer/Dockerfile.R
@@ -0,0 +1,31 @@
+# Base Image
+FROM rocker/r-ver:4.3.0
+
+# Install system dependencies
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+    libudunits2-dev \
+    libgdal-dev \
+    libcurl4-openssl-dev \
+    libssl-dev \
+    libxml2-dev \
+    libproj-dev \
+    build-essential \
+    git \
+    curl && \
+    rm -rf /var/lib/apt/lists/*
+
+# Set working directory to /app
+WORKDIR /app
+
+# Copy the R package or the current directory into the container
+COPY . /app
+
+# Install R package dependencies
+RUN R -e "install.packages(c('devtools', 'roxygen2', 'testthat'))"
+
+# Install the R package
+RUN R -e "devtools::install('.', dependencies=TRUE, keep_source=TRUE)"
+
+# Default command
+CMD ["R"]
