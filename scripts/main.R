4d0936640043272bdf2a33b7a5354f8382042a23
IshikaKhandelwal <69579617+IshikaKhandelwal@users.noreply.github.com>
2025-02-27 15:38:28 -0800

    feat: add command line executables for climatearray2rds processing (#93)
    * feat: add command line executables for climatearray2rds processing
    
    * feat: add command line executables for climatearray2rds processing
    
    * add docker_climatearray2rds.sh

--- scripts/main.R
+++ scripts/main.R
@@ -130,22 +130,71 @@ run_shp2rds <- function(args) {
   cat("Parallel:", opt$parallel, "\n")
   cat("Workers:", opt$workers, "\n")
 
-  grid <- create_grid(
-    extent_vals = extent,
-    resolution = opt$resolution,
-    crs = opt$crs
-  )
-  range_data <- prepare_range_data_from_shp_file(
-    input_file_path = opt$input,
-    grid = grid,
-    realm = opt$realm,
-    use_parallel = opt$parallel,
-    number_of_workers = opt$workers,
-    rds_output_file_path = opt$output
-  )
+  grid <- create_grid(extent_vals = extent,
+                      resolution = opt$resolution,
+                      crs = opt$crs)
+  range_data <- prepare_range_data_from_shp_file(input_file_path = opt$input,
+                                                 grid = grid,
+                                                 realm = opt$realm,
+                                                 use_parallel = opt$parallel,
+                                                 number_of_workers = opt$workers,
+                                                 rds_output_file_path = opt$output)
   print("File converted successfully!")
 }
 
+run_climatearray2rds <- function(args) {
+  source("utility/convert_array_to_raster.R")
+
+  option_list <- list(
+    make_option(c("-i", "--input"),
+      type = "character",
+      help = "Directory containing .rds climate array files"
+    ),
+    make_option(c("-o", "--output"),
+      type = "character",
+      help = "Directory to save processed raster .rds files"
+    ),
+    make_option(c("-y", "--start_year"),
+      type = "integer",
+      default = 1961,
+      help = "Starting year for naming convention (default: 1961)"
+    ),
+    make_option(c("-p", "--parallel"),
+      type = "logical",
+      help = "Use parallel processing. Default is TRUE.",
+      default = TRUE
+    ),
+    make_option(c("-w", "--workers"),
+      type = "numeric",
+      help = "Number of workers to use. Default is availableCores()-1.",
+      default = availableCores()-1
+    )
+  )
+
+  # Parse command-line arguments
+  opt <- safe_parse_opts(OptionParser(option_list = option_list), args[-1])
+  check_not_null(opt$input, "input")
+  check_not_null(opt$output, "output")
+
+  # Print the received arguments
+  print("Processing climate array .rds files with the following options:")
+  cat("Input Directory:", opt$input, "\n")
+  cat("Output Directory:", opt$output, "\n")
+  cat("Start Year:", opt$start_year, "\n")
+  cat("Parallel:", opt$parallel, "\n")
+  cat("Workers:", opt$workers, "\n")
+
+  # Run the climate array processing function
+  processed_raster <- process_climate_array_data(
+    input_dir = opt$input,
+    output_dir = opt$output,
+    start_year = opt$start_year,
+    use_parallel = opt$parallel,
+    number_of_workers = opt$workers
+  )
+
+  print("File converted successfully!")
+}
 
 run_exposure <- function(args) {
   source("scripts/exposure_workflow.R")
@@ -188,6 +237,8 @@ if (cmd == "shp2rds") {
   run_exposure(args)
 } else if (cmd == "tif2rds") {
   run_tif2rds(args)
+} else if (cmd == "climatearray2rds") {
+  run_climatearray2rds(args)
 } else {
-  stop("Invalid command. Use 'shp2rds', 'tif2rds' or 'exposure'.")
+  stop("Invalid command. Use 'shp2rds', 'tif2rds', 'climatearray2rds' or 'exposure'.")
 }
