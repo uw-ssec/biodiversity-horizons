f3bcaf5f952233b2bc302c3ca2f7c040cb797384
Anuj Sinha <sinha.anuj30@gmail.com>
2025-03-26 10:42:12 -0700

    fix: update docker image name to be same as package name

--- run_container.sh
+++ run_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # CONTAINER=biodiv # for development, change this to the name of your local Docker ccontainer build
-CONTAINER=ghcr.io/uw-ssec/biodiversity-horizons
+CONTAINER=ghcr.io/uw-ssec/biodiversityhorizons
 
 function run_with_mounts() {
 
